e6214c4a2268b1c3bf2959ea25b412c9aa2055f8
olivier <olivier.catanese@gmail.com>
2022-07-26 18:26:05 +0200

    Maj CodesResultatsBLL et CodesResultatsServlets

--- src/main/java/fr/eni/projetenchere/bll/CodesResultatBLL.java
+++ src/main/java/fr/eni/projetenchere/bll/CodesResultatBLL.java
@@ -8,15 +8,17 @@ public abstract class CodesResultatBLL {
 	/**
 	 * Echec les informations de l'utilisateur ne respectent pas les règles définies
 	 */
-	public static final int REGLE_UTILISATEUR_PSEUDO_ERREUR = 20000;
-	public static final int REGLE_UTILISATEUR_NOM_ERREUR=20001;
-	public static final int REGLE_UTILISATEUR_PRENOM_ERREUR=20002;
-	public static final int REGLE_UTILISATEUR_EMAIL_ERREUR=20003;
-	public static final int REGLE_UTILISATEUR_TELEPHONE_ERREUR=20004;
-	public static final int REGLE_UTILISATEUR_RUE_ERREUR=20005;
-	public static final int REGLE_UTILISATEUR_CODE_POSTAL_ERREUR=20006;
-	public static final int REGLE_UTILISATEUR_VILLE_ERREUR=20007;
-	public static final int REGLE_UTILISATEUR_MDP_ERREUR=20008;
-
-	
+	public static final int REGLE_UTILISATEUR_NULL_ERREUR = 20000;
+	public static final int REGLE_UTILISATEUR_PSEUDO_ERREUR = 20001;
+	public static final int REGLE_UTILISATEUR_NOM_ERREUR=20002;
+	public static final int REGLE_UTILISATEUR_PRENOM_ERREUR=20003;
+	public static final int REGLE_UTILISATEUR_EMAIL_ERREUR=20004;
+	public static final int REGLE_UTILISATEUR_TELEPHONE_ERREUR=20005;
+	public static final int REGLE_UTILISATEUR_RUE_ERREUR=20006;
+	public static final int REGLE_UTILISATEUR_CODE_POSTAL_ERREUR=20007;
+	public static final int REGLE_UTILISATEUR_VILLE_ERREUR=20008;
+	public static final int REGLE_UTILISATEUR_MDP_ERREUR=20009;
+	public static final int REGLE_UTILISATEUR_CREDIT_ERREUR=20011;
+	public static final int REGLE_UTILISATEUR_CONFIRMATION_ERREUR=20010;
+	public static final int VALIDATION_UTILISATEUR_ERREUR = 20012;
 }
--- src/main/java/fr/eni/projetenchere/servlet/CodesResultatServlets.java
+++ src/main/java/fr/eni/projetenchere/servlet/CodesResultatServlets.java
@@ -8,12 +8,8 @@ public abstract class CodesResultatServlets {
 	/**
 	 * Format id liste course incorrect
 	 */
-	public static final int FORMAT_ID_LISTE_ERREUR=30000;
-	/**
-	 * Format id liste course incorrect
-	 */
+	public static final int SERVLET_ERREUR=30000;
 
-	public static final Integer VALIDATION_UTILISATEUR_ERREUR = 30000;
 	
 	
 	
