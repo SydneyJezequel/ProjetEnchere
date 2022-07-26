7b09588b724dbe677750186a793b5af69b3278d2
Sydney Jezequel <107682562+SydneyJezequel@users.noreply.github.com>
2022-07-26 09:42:13 +0200

    maj UtilisateurDAOJdbcImpl (Insert)

--- src/main/java/fr/eni/projetenchere/dal/UtilisateurDAOJdbcImpl.java
+++ src/main/java/fr/eni/projetenchere/dal/UtilisateurDAOJdbcImpl.java
@@ -16,8 +16,9 @@ public class UtilisateurDAOJdbcImpl implements UtilisateurDAO {
 	// Requêtes paramétrées :
 	private static final String SELECT_BY_ID = "SELECT no_utilisateur, pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur FROM UTILISATEURS WHERE no_utilisateur = ?";
 	private static final String SELECT_BY_NAME = "SELECT no_utilisateur, pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur FROM UTILISATEURS WHERE pseudo = ?";
-	private static final String UPDATE_UTILISATEURS = "UPDATE UTILISATEURS SET pseudo = ? , nom= ? , prenom = ? , email = ? , telephone = ? , rue = ? , code_postal = ? , ville = ? , mot_de_passe = ? WHERE no_utilisateur = ?";
+	private static final String UPDATE_UTILISATEURS = "UPDATE utilisateurs SET pseudo = ? , nom= ? , prenom = ? , email = ? , telephone = ? , rue = ? , code_postal = ? , ville = ? , mot_de_passe = ? WHERE no_utilisateur = ?";
 
+	
 	
 	
 	/*-------------------------------------- METHODES -------------------------------------- */
