65b47400a7c18191c494401d60c273c63e2f7e4e
Sydney Jezequel <107682562+SydneyJezequel@users.noreply.github.com>
2022-07-28 17:27:22 +0200

    Maj ServletInscription

--- src/main/java/fr/eni/projetenchere/servlet/ServletInscription.java
+++ src/main/java/fr/eni/projetenchere/servlet/ServletInscription.java
@@ -3,14 +3,12 @@ package fr.eni.projetenchere.servlet;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.time.LocalDate;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import fr.eni.projetenchere.BusinessException;
 import fr.eni.projetenchere.bll.BLLException;
 import fr.eni.projetenchere.bll.UtilisateurManager;
